3b07e757baa5e425220e688a697f12c405248b50
YuviPanda <yuvipanda@gmail.com>
2021-02-13 22:14:35 +0530

    Remove upload dialog box
    No need to have an extra piece of UI

--- paste/upload.js
+++ paste/upload.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     let dialogButton = document.getElementById('paste-UploadButton');
-    let dialogClose = document.getElementById('paste-UploadDialog-Close');
-    let dialog = document.getElementById('paste-UploadDialog');
     let dialogUpload = document.getElementById('paste-UploadFile');
     let dialogForm = document.getElementById('paste-UploadForm');
 
     dialogButton.addEventListener('click', () => {
-        dialog.classList.remove('paste-Hidden');
-    })
-
-    dialogClose.addEventListener('click', () => {
-        dialog.classList.add('paste-Hidden');
+        dialogUpload.click();
     })
 
     dialogUpload.addEventListener('change', () => {
