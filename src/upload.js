ad75f4a1819e3ef85f3cfad5cf3bbb08b46250b3
YuviPanda <yuvipanda@gmail.com>
2021-04-09 21:29:24 +0530

    Add some feedback when uploading notebook
    Clear to me this needs to have some kinda framework.

--- src/upload.js
+++ src/upload.js
@@ -1,8 +1,32 @@
+const uploadStateNormal = document.getElementById('action-upload-state-normal');
+const uploadStateProgress = document.getElementById('action-upload-state-progress');
+
+const UPLOAD_STATES = {
+    NORMAL: 1,
+    IN_PROGRESS: 2,
+}
+
+function setUploadState(state) {
+    switch(state) {
+        case UPLOAD_STATES.NORMAL:
+            uploadStateNormal.classList.remove('hidden');
+            uploadStateProgress.classList.add('hidden');
+            break;
+        case UPLOAD_STATES.IN_PROGRESS:
+            uploadStateProgress.classList.remove('hidden');
+            uploadStateNormal.classList.add('hidden');
+            break;
+
+    }
+}
+
 function uploadFile(file) {
     let formData = new FormData();
 
     formData.append("notebook", file);
+
     // FIXME: Error handling
+    setUploadState(UPLOAD_STATES.IN_PROGRESS);
     fetch('/upload', {
         method: "POST",
         body: formData,
