64ac961c4a1fa118bbad47d6f4a8bfece0bda61c
YuviPanda <yuvipanda@gmail.com>
2021-04-10 12:43:18 +0530

    Format with black

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from nbconvert.exporters import HTMLExporter
+
 from typing import Optional
 import os
 import nbformat
@@ -10,55 +11,63 @@ from fastapi.templating import Jinja2Templates
 
 from storage import S3Backend
 
-app = FastAPI(root_path='/')
+app = FastAPI(root_path="/")
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
-DATA_DIR = os.environ.get('DATA_DIR', os.getcwd())
+DATA_DIR = os.environ.get("DATA_DIR", os.getcwd())
 
-templates = Jinja2Templates(directory=os.path.join(BASE_PATH, 'templates'))
-app.mount("/static", StaticFiles(directory=os.path.join(BASE_PATH, "static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(BASE_PATH, "templates"))
+app.mount(
+    "/static", StaticFiles(directory=os.path.join(BASE_PATH, "static")), name="static"
+)
 
 backend = S3Backend()
 
 
 @app.post("/upload")
-async def upload(notebook: UploadFile = File(...), host: Optional[str] = Header(None), x_forwarded_proto: str = Header('http'), accept: str = Header('text/plain')):
+async def upload(
+    notebook: UploadFile = File(...),
+    host: Optional[str] = Header(None),
+    x_forwarded_proto: str = Header("http"),
+    accept: str = Header("text/plain"),
+):
     data = await notebook.read()
 
     name = await backend.put(data)
 
     # FIXME: is this really the best way?
-    url = f'{x_forwarded_proto}://{host}{app.root_path}view/{name}'
-    if accept == 'application/json':
-        return {
-            'url': url
-        }
+    url = f"{x_forwarded_proto}://{host}{app.root_path}view/{name}"
+    if accept == "application/json":
+        return {"url": url}
 
     else:
-        return Response(url + '\n')
+        return Response(url + "\n")
 
 
-@app.get('/view/{name}')
+@app.get("/view/{name}")
 async def view(name: str, request: Request, download: bool = False):
     if download:
-        return Response(await backend.get(name), headers={
-            "Content-Type": "application/json",
-            "Content-Disposition": f'attachment; filename={name}.ipynb'
-        })
+        return Response(
+            await backend.get(name),
+            headers={
+                "Content-Type": "application/json",
+                "Content-Disposition": f"attachment; filename={name}.ipynb",
+            },
+        )
     return templates.TemplateResponse(
-        'view.html.j2', { 'name': name, 'request': request }
+        "view.html.j2", {"name": name, "request": request}
     )
 
-@app.get('/render/v1/{name}')
+
+@app.get("/render/v1/{name}")
 async def render(name: str):
     exporter = HTMLExporter(
         # Input / output prompts are empty left gutter space
         # Let's remove them. If we want gutters, we can CSS them.
         exclude_input_prompt=True,
         exclude_output_prompt=True,
-
         extra_template_basedirs=[BASE_PATH],
-        template_name='nbconvert-template'
+        template_name="nbconvert-template",
     )
     data = await backend.get(name)
     if data is None:
@@ -66,15 +75,17 @@ async def render(name: str):
         raise HTTPException(status_code=404)
     notebook = nbformat.reads(data, as_version=4)
     output, resources = exporter.from_notebook_node(notebook, {})
-    return HTMLResponse(output, headers={
-        # Disable embedding our rendered notebook in other websites
-        # Don't want folks hotlinking our renders.
-        'Content-Security-Policy': "frame-ancestors 'self';",
-        'X-Frame-Options': 'SAMEORIGIN'
-    })
+    return HTMLResponse(
+        output,
+        headers={
+            # Disable embedding our rendered notebook in other websites
+            # Don't want folks hotlinking our renders.
+            "Content-Security-Policy": "frame-ancestors 'self';",
+            "X-Frame-Options": "SAMEORIGIN",
+        },
+    )
 
-@app.get('/')
+
+@app.get("/")
 async def render_front(request: Request):
-    return templates.TemplateResponse('front.html.j2', {
-        'request': request
-    })
+    return templates.TemplateResponse("front.html.j2", {"request": request})
--- storage.py
+++ storage.py
@@ -10,12 +10,16 @@ import gzip
 import aioboto3
 from yarl import URL
 import hashlib
+import BaseEmoji
+
+anybase = BaseEmoji.anybase()
 
 
 def sha256(data: bytes):
     digester = hashlib.sha256()
     digester.update(data)
-    return digester.hexdigest()
+    return anybase.encode(digester.digest())
+
 
 class StorageBackend:
     async def put(self, name: str, path: str):
@@ -24,16 +28,17 @@ class StorageBackend:
     async def get(self, name: str) -> str:
         pass
 
+
 class FileBackend(StorageBackend):
     def __init__(self):
-        self.data_path = os.environ.get('DATA_DIR', os.getcwd())
+        self.data_path = os.environ.get("DATA_DIR", os.getcwd())
 
     def path_for_name(self, name: str) -> str:
         return os.path.join(self.data_path, name)
 
     async def put(self, data: bytes):
         name = sha256(data)
-        with gzip.open(self.path_for_name(name), 'w') as f:
+        with gzip.open(self.path_for_name(name), "w") as f:
             f.write(data)
         return name
 
@@ -44,64 +49,57 @@ class FileBackend(StorageBackend):
 
 class S3Backend(StorageBackend):
     def __init__(self):
-        self.endpoint_url = os.environ.get('AWS_S3_ENDPOINT_URL')
-        self.bucket = os.environ['AWS_S3_BUCKET']
+        self.endpoint_url = os.environ.get("AWS_S3_ENDPOINT_URL")
+        self.bucket = os.environ["AWS_S3_BUCKET"]
 
     def path_for_name(self, name: str) -> str:
-        return f'notebooks/{name}'
+        return f"notebooks/{name}"
 
     async def put(self, data: bytes):
         name = sha256(data)
-        async with aioboto3.client('s3', endpoint_url=self.endpoint_url) as s3:
+        async with aioboto3.client("s3", endpoint_url=self.endpoint_url) as s3:
             await s3.put_object(
                 Key=self.path_for_name(name),
                 Bucket=self.bucket,
-                Body=gzip.compress(data)
+                Body=gzip.compress(data),
             )
         return name
 
-
     async def get(self, name: str):
-        async with aioboto3.client('s3', endpoint_url=self.endpoint_url) as s3:
+        async with aioboto3.client("s3", endpoint_url=self.endpoint_url) as s3:
 
             try:
-                gzip_response = await (await s3.get_object(
-                    Key=self.path_for_name(name),
-                    Bucket=self.bucket,
-                ))['Body'].read()
+                gzip_response = await (
+                    await s3.get_object(
+                        Key=self.path_for_name(name),
+                        Bucket=self.bucket,
+                    )
+                )["Body"].read()
             except s3.exceptions.NoSuchKey:
                 return None
             return gzip.decompress(gzip_response)
 
 
 class IPFSBackend(StorageBackend):
-    def __init__(self, daemon_url='http://localhost:5001'):
+    def __init__(self, daemon_url="http://localhost:5001"):
         self.client = aiohttp.ClientSession()
         self.daemon_url = URL(daemon_url)
 
     # FIXME: MAKE ALL THIS ASYNC AAAAA
     async def put(self, data: bytes):
-        add_url = self.daemon_url / 'api/v0/add' % {
-            'cid-version': 1,
-            'pin': 'true'
-        }
+        add_url = self.daemon_url / "api/v0/add" % {"cid-version": 1, "pin": "true"}
 
-        files = {
-            'notebook.ipynb': data
-        }
+        files = {"notebook.ipynb": data}
 
         resp = await self.client.post(add_url, data=files)
-        cid = (await resp.json())['Hash']
+        cid = (await resp.json())["Hash"]
 
         return cid
 
     async def get(self, name: str):
-        url = self.daemon_url / 'api/v0/cat' % {'arg': name}
+        url = self.daemon_url / "api/v0/cat" % {"arg": name}
 
         resp = await self.client.post(url)
         if resp.status != 200:
-            raise HTTPException(
-                status_code=resp.status,
-                detail=(await resp.text())
-            )
+            raise HTTPException(status_code=resp.status, detail=(await resp.text()))
         return await resp.read()
