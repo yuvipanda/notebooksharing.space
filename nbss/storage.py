1188391599cf7a2e0fcd8cf0a5737ef850b3062b
YuviPanda <yuvipanda@gmail.com>
2021-10-09 15:12:15 +0530

    Provide correct type hint for tuple returns
    Whatever vscode uses by default gave me this helpful hint!

--- nbss/storage.py
+++ nbss/storage.py
@@ -6,6 +6,7 @@ import json
 import gzip
 import aioboto3
 import hashlib
+from typing import Tuple
 
 
 def sha256(data: bytes, raw_metadata: dict):
@@ -52,7 +53,7 @@ class StorageBackend:
     async def put(self, data: bytes, raw_metadata: dict):
         pass
 
-    async def get(self, name: str) -> (bytes, Metadata):
+    async def get(self, name: str) -> Tuple(bytes, Metadata):
         pass
 
 
@@ -83,7 +84,7 @@ class FileBackend(StorageBackend):
 
         return Metadata.from_dict(raw_metadata)
 
-    async def get(self, name: str) -> (bytes, Metadata):
+    async def get(self, name: str) -> Tuple(bytes, Metadata):
         with gzip.open(self.data_path_for_name(name)) as f:
             data = f.read()
 
@@ -120,7 +121,7 @@ class S3Backend(StorageBackend):
                 return None
             return metadata
 
-    async def get(self, name: str) -> (bytes, Metadata):
+    async def get(self, name: str) -> Tuple(bytes, Metadata):
         async with aioboto3.client("s3", endpoint_url=self.endpoint_url) as s3:
 
             try:
