87d1f6238065d34d8628fc38424db250965b34ee
YuviPanda <yuvipanda@gmail.com>
2021-10-20 14:51:26 +0530

    Fix typo

--- deploy.bash
+++ deploy.bash
@@ -4,6 +4,6 @@ set -euo pipefail
 git push origin main
 
 helm upgrade --install --namespace=nbss --create-namespace \
-	nbss helm-chart/nbss \
+	nbss helm/nbss \
 	--set env.AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
 	--set env.AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
