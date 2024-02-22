617a4dc52aff664d9bb6ee0535240477326355fb
YuviPanda <yuvipanda@gmail.com>
2024-02-22 11:20:59 -0800

    Increase size limit to 100MB

--- nbss/app.py
+++ nbss/app.py
@@ -64,8 +64,8 @@ app.mount(
     StaticFiles(directory=os.path.join(BASE_PATH, "static/jupyterlite"), html=True),
     name="jupyterlite",
 )
-# No files larger than 10MB
-app.add_middleware(ContentSizeLimitMiddleware, max_content_size=10 * 1024 * 1024)
+# No files larger than 100MB
+app.add_middleware(ContentSizeLimitMiddleware, max_content_size=100 * 1024 * 1024)
 
 backend = S3Backend()
 # Basic prometheus metrics
